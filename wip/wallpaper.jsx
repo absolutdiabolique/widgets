d7f536bc73c45510a82f570cb6c281f4aac3acbb
absolutdiabolique <personal.ronchen@gmail.com>
2025-11-12 09:20:50 -0800

    Update wallpaper.jsx

--- wip/wallpaper.jsx
+++ wip/wallpaper.jsx
@@ -1,1 +1,19 @@
+// wallpaper.jsx
+// Animated wallpaper
 
+export const command = () => "";
+export const refreshFrequency = False;
+
+export const render = () => {
+  const containerStyle = {
+    width: '100%',
+    height: '100%',
+  };
+
+  return (
+    <div
+      ref={(el) => (element = el)}
+      style={containerStyle}
+    ></div>
+  );
+};
